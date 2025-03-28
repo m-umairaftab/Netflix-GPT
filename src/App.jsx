28b144ce98792b2239dd4ed82418fb37ef046ef0
Umair Aftab <m.umair.umairAftab@gmail.com>
2025-03-28 15:44:33 +0500

    sign in and sign up page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
+import Body from "./components/Body";
+
 export default function App() {
-  return (
-    <div className="flex h-screen items-center justify-center bg-black">
-      <h1 className="text-5xl font-extrabold text-yellow-500 underline">
-        Tailwind CSS is Working! 🚀
-      </h1>
-    </div>
-  );
+  return <Body />;
 }
---
+++ src/components/Login.jsx
@@ -0,0 +1,79 @@
+import React, { useRef, useState } from "react";
+import Header from "./Header";
+import { checkValidData } from "../utils/validate";
+
+const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const email = useRef(null);
+  const password = useRef(null);
+  const name = useRef(null);
+
+  const handleClickButton = () => {
+    const message = checkValidData(email.current.value, password.current.value);
+    setErrorMessage(message);
+    if (message) return;
+  };
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
+
+  return (
+    <div>
+      <Header />
+      <div className="absolute">
+        <img
+          className="h-screen object-cover"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          alt="BE img"
+        />
+      </div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+        className="w-full md:w-3/12 absolute p-12 bg-black/90 my-36 mx-auto right-0 left-0 text-white rounded-lg"
+      >
+        <h1 className="font-bold text-3xl py-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+            ref={name}
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-4 w-full bg-gray-700"
+          />
+        )}
+
+        <input
+          ref={email}
+          type="text"
+          placeholder="Email Address"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <input
+          ref={password}
+          type="password"
+          placeholder="Password"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <p className="text-red-500 font-bold text-lg py-2">{errorMessage}</p>
+        <button
+          className="p-4 my-6 bg-red-700 w-full rounded-lg cursor-pointer"
+          onClick={handleClickButton}
+        >
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
+          {isSignInForm
+            ? "New to Netflix? Sign Up Now"
+            : "Already registered? Sign In Now."}
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
