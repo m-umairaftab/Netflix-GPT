ac5c34edd22cba8ed891d60cf658e463c9718b5b
Umair Aftab <m.umair.umairAftab@gmail.com>
2025-03-28 10:36:47 +0500

    setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-black">
+      <h1 className="text-5xl font-extrabold text-yellow-500 underline">
+        Tailwind CSS is Working! 🚀
+      </h1>
+    </div>
+  );
+}
